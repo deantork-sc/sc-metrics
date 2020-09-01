18b189e6c2bebc373b4d5e12381d996fdd78a2dc
Dean Torkelson <dean.torkelson@silvercar.com>
2020-09-01 15:46:20 -0500

    first pass at lead time via github metrics

--- README.md
+++ README.md
@@ -23,36 +23,30 @@ Want to group by scrum team and individual
 #### Goal
 Get the time it takes between start of work on a story and that story's deployment to the code
 #### Ideas
-If all tickets are closed as soon as they are merged into master, just get time discrepancy between when issue was opened and when it was closed
+If all tickets are closed as soon as they are merged into master, just get time discrepancy between first and last commit
 #### Programatic solution
-* [Get all issues](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-issueidorkey-get) for a team in a given timespan
-* Filter the issues to only see the closed issues
-* Look at each [issue's changelog](https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issues/#api-rest-api-3-issue-issueidorkey-changelog-get) to get time of assignment and time that status was set to done
-* Find the difference between the two 
-* Find the average of these differences for a team/individual
+* From GitHub, get the last 100 successful PRs in some repo (mob-api? dw-web?)
+* Get the diff between the timestamp of the first commit and the timestamp of when the PR was merged
+* From the description, scrape the story title (optional, for debugging)
 #### Potential bugs
-* Timezones
-* Do all teams have the same workflow with how they handle ticket statuses?
-    temporary fix: don't check for "done", check for "resolution"
 
 ## MTTR
 #### Goal
 Get the percentage of the time we experience a service outage
 #### Ideas
+Use AWS MTTR
 #### Programatic solution
 
 ## Deployment frequency
 #### Goal
 Get the average number of releases per developer, per day
 #### Ideas
+Does CircleCI provide enough correct information? AWS?
 #### Programatic solution
 
 ## Change fail percentage
 #### Goal
-Find the percentage of master builds that fail
+Find the number of Highest/High priority bugs (type = bug/support) between the previous build and the current build, per build
 #### Ideas
-For some number of builds on master, find what percentage failed compared to the percentage that succeeded
+
 #### Programatic solution
-* Get [some number of builds](https://circleci.com/docs/api/#recent-builds-for-a-single-project) (30? 100?) for the project
-* Filter these results to only be builds on master that either succeeded or failed
-* Calculate the ratio of failed builds to total
--- github_api.py
+++ github_api.py
@@ -1,21 +1,78 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import datetime
 
 class GithubApi:
     def __init__(self):
         with open('./config.json') as config:
             data = json.load(config)
             token = data["GITHUB_PAT"]
+        self.debug = True
         self.headers = { 
-            "Accept": "application/json",
+            "Accept": "Accept: application/vnd.github.v3+json",
             "Authorization": f"token {token}" }
+    
+    def time_delta(self, start_datetime, end_datetime):
+        format_string = "%Y-%m-%dT%H:%M:%SZ"
+        start_datetime_obj = datetime.datetime.strptime(start_datetime, format_string)
+        end_datetime_obj = datetime.datetime.strptime(end_datetime, format_string)
+        delta = end_datetime_obj - start_datetime_obj
+        # hours not stored in delta object - need to derive from seconds
+        return delta.days * 24 + delta.seconds/3600
 
-    def get_commits(self):
-        url = f"https://api.github.com/repos/silvercar/dw-vin-decoder/commits"
+    def get_prs(self, project, state, limit):
+        url = f"https://api.github.com/repos/silvercar/{project}/pulls?state={state}&per_page={limit}"
         return requests.request( "GET", url, headers=self.headers)
 
+    def get_lead_time_array(self, limit, project):
+        prs_json = json.loads(self.get_prs(project, "closed", limit).text)
+        delta_list = []
+        bugfix_count = 0
+        release_count = 0
+        other_count = 0
+        unlabeled_count = 0
+        for pr in prs_json:
+            if (self.debug):
+                print(pr["title"])
+                print("  Body: " + repr(pr["body"][:80]) + "...")
+            if not ("[x] Feature" in pr["body"]):
+                if "[x] Bug Fix" in pr["body"]:
+                    bugfix_count += 1
+                if "[x] Release" in pr["body"]:
+                    release_count += 1
+                if "[x] Other" in pr["body"]:
+                    other_count += 1
+                if not ("[x]" in pr["body"][:80]):
+                    unlabeled_count += 1
+                    if (self.debug):
+                        print("unlabeled: " + pr["title"])
+                continue
+            commits_response = requests.request("GET", url=pr["commits_url"], headers=self.headers)
+            commits_json = json.loads(commits_response.text)
+            # commits_json is an array of commit objects, from oldest as first elem to most recent as last elem
+            start_datetime = commits_json[0]["commit"]["committer"]["date"]
+            end_datetime = commits_json[-1]["commit"]["committer"]["date"]
+            if (self.debug):
+                print(f"  start={start_datetime}, end={end_datetime}")
+            delta = self.time_delta(start_datetime, end_datetime)
+            delta_list.append(delta)
+        print(f"  In the last {limit} PRs, found {len(delta_list)} features, {bugfix_count} bugfixes, {release_count} releases, {unlabeled_count} unlabeled, and {other_count} misc. others.")
+        return delta_list
+
+    def metrics(self, lead_time_count, project):
+        print(f"Examining metrics for {project}")
+        delta_list = self.get_lead_time_array(lead_time_count, project)
+        count = len(delta_list)
+        if count == 0:
+            print ("No PRs found with current criteria.")
+            return
+        average = sum(delta_list)
+        delta_list.sort()
+        median = delta_list[int(count / 2)]
+        print(f"Lead time metrics (in hours) for the last {count} feature PRs in {project}: ")
+        print(f"  Median: {median}")
+        print(f"  Average: {average}")
 
 github = GithubApi()
-response = github.get_commits()
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+response = github.metrics(50, "dw-web")
